6d831c2012970418a53835bd1cdecf46c57d12c1
Haakon Langballe <Haakon.Langballe@abgsc.no>
2020-06-08 15:46:26 +0200

    Added project files

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace word_mailmerge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Merger mrg = new Merger();
+            mrg.Merge4();
+
+        }
+    }
+}
